5456faca736e5b5423ffdaaaf599ccd089fee69a
Breny Kurien <brenykurien007@gmail.com>
2019-09-16 15:52:09 +0530

    changed number of services

--- common/enums/Service.py
+++ common/enums/Service.py
@@ -3,10 +3,7 @@ from enum import IntEnum
 
 class Service(IntEnum):
     LULC205KDataset = 0
-    GeoWebCacheWMS = 1
-    BhuvanV1WMS = 2
-    BhuvanV2WMS = 3
-    FloodAnnualLayers = 4
-    FloodHazard = 5
-    BhuvanV1WMTS = 6
-    BhuvanV2WMTS = 7
+    BhuvanV1WMS = 1
+    BhuvanV2WMS = 2
+    BhuvanV1WMTS = 3
+    BhuvanV2WMTS = 4
